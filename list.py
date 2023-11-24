8cf369f5fcec58b5d0282643f01885ae58312ec0
Shimu1 <2020-1-60-157@std.ewubd.edu>
2023-11-24 21:21:09 +0600

    update in list file but in featureBranch1

---
+++ list.py
@@ -0,0 +1,10 @@
+#python _list
+thislist = ["apple","grapes", "banana","orange", "cherry","pinaples"]
+thislist.append("lichi")
+print(thislist)
+#change list
+thislist[1] = "blackcurrant"
+print(thislist)
+#remove list
+thislist.remove("banana")
+print(thislist)
