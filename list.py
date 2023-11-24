7cbd2a7ba094aa8f049de315df3f8fc8ee040c8e
Shimu1 <2020-1-60-157@std.ewubd.edu>
2023-11-24 21:18:52 +0600

    create a list flie in dev

---
+++ list.py
@@ -0,0 +1,10 @@
+#python _list
+thislist = ["apple","grapes", "banana", "cherry","pinaples"]
+thislist.append("orange")
+print(thislist)
+#change list
+thislist[1] = "blackcurrant"
+print(thislist)
+#remove list
+thislist.remove("banana")
+print(thislist)
